c334dc364b388021e91115d2e2e3aa3c0902b841
k4ssyi <kasikasi00@gmail.com>
2022-07-07 20:57:17 +0900

    feat: backend TodoTask

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+app.use(express.json()); // json形式を元にexpressを使う
 
 const PORT = 5000;
 
 // ルーティング設計
 app.use("/api/v1/tasks", taskRoute);
 
-app.listen(PORT, console.log("server listening on port " + PORT));
+// データベースと接続
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, console.log("server listening on port " + PORT));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
